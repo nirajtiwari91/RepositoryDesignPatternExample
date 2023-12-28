34783af3197bfc2c8815ba66463b39b8d62678ac
Niraj <nirajtiwari91@gmail.com>
2023-12-28 15:46:09 +0530

    One changes

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<UserServiceRepository, >();
+builder.Services.AddScoped<UserServiceRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
